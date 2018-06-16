2a2d2112c030bf94734d0c0c13b240fef661eabf
Jonathan Shahen <jonathan.shahen@gmail.com>
2018-06-15 20:50:04 -0400

    Moving around the starter files (examples folder); adding a new folder to give the students sample code to work off of

---
+++ lab3/starter/lab3/produce.c
@@ -0,0 +1,50 @@
+// Use this to see if a number has an integer square root
+#define EPS 1.E-7
+
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mqueue.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <math.h>
+
+double g_time[2];
+
+
+int main(int argc, char *argv[])
+{
+	int num;
+	int maxmsg;
+	int num_p;
+	int num_c;
+	int i;
+	struct timeval tv;
+
+	if (argc != 5) {
+		printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
+		exit(1);
+	}
+
+	num = atoi(argv[1]);	/* number of items to produce */
+	maxmsg = atoi(argv[2]); /* buffer size                */
+	num_p = atoi(argv[3]);  /* number of producers        */
+	num_c = atoi(argv[4]);  /* number of consumers        */
+
+
+	gettimeofday(&tv, NULL);
+	g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
+
+
+    gettimeofday(&tv, NULL);
+    g_time[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
+
+    printf("System execution time: %.6lf seconds\n", \
+            g_time[1] - g_time[0]);
+	exit(0);
+}
