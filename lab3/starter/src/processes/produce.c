876ff652736f028dcd0073d736428ebc42bd6529
tanay97 <tanaydesai1997@gmail.com>
2018-11-22 15:33:32 -0500

    added kill code function, only kill after done producing

--- lab3/starter/src/processes/produce.c
+++ lab3/starter/src/processes/produce.c
@@ -29,6 +29,27 @@ int isPerfectSquare(int number)
 	return 0;
 }
 
+int send_kill_codes (int num_c, int maxmsg){
+	
+	mqd_t qdes;
+	char *qname = "/q";
+	mode_t mode = S_IRUSR | S_IWUSR;
+	struct mq_attr attr;
+	
+	attr.mq_maxmsg = maxmsg;
+	attr.mq_msgsize = sizeof (int);
+	attr.mq_flags = 0;
+	qdes = mq_open(qname, O_RDWR | O_CREAT, mode, &attr);
+
+	int kill_code = -1;
+	for(int i = 0; i < num_c; i++){	
+		mq_send(qdes, (char *)&kill_code, sizeof(int), 0);
+		//printf("sending kill code by %d \n", pid);
+	}		
+	return 0;
+
+}
+
 int produce(int pid, int num_p, int num_c, int maxmsg, int num){
 	mqd_t qdes;
 	char *qname = "/q";
@@ -51,10 +72,6 @@ int produce(int pid, int num_p, int num_c, int maxmsg, int num){
 			}
 			//printf("producing %d by %d \n", i, pid);
 		}
-	}
-	int kill_code = -1;
-	for(int i = 0; i < num_c; i++){
-		mq_send(qdes, (char*)&kill_code, sizeof(int), 0); 
 	}
 	mq_close(qdes);
 	return 0;	
@@ -85,10 +102,10 @@ int consume(int cid, int maxmsg){
 		int res = mq_receive(qdes, (char *) &p, sizeof(int), 0);
 		if (p == -1){
 			loop = 0;
-			//printf("killing %d \n", cid);
 	
 		}
 		else{
+			printf("received %d \n", p);
 			if (isPerfectSquare(p)){
 				printf("%d %d %d \n", cid, p, (int) sqrt(p));
 			}
@@ -96,7 +113,6 @@ int consume(int cid, int maxmsg){
 			
 		//mq_receive(qdes, (char *) &p, sizeof(int), 0);
 		
-		//printf("received %d \n", p);
 	}
 	mq_close(qdes);
 	return 0;
@@ -124,15 +140,16 @@ int main(int argc, char *argv[])
 
 	int status = 0;
 	
-	pid_t pid, wpid;
-
+	pid_t p_pids[num_p];
+	pid_t c_pids[num_c];
+	
 	gettimeofday(&tv, NULL);
 	g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
 
 			
 	for(int i = 0; i < num_p; i++){
-		pid = fork();
-		if (pid == 0){
+		p_pids[i] = fork();
+		if (p_pids[i] == 0){
 			//printf("producer created %d \n", i);
 			produce(i, num_p, num_c, maxmsg, num);
 			exit(0);	
@@ -140,33 +157,27 @@ int main(int argc, char *argv[])
 	}
 	
 	for(int i = 0; i < num_c; i++){
-		pid = fork();
-		if (pid == 0){
+		c_pids[i] = fork();
+		if (c_pids[i] == 0){
 			//printf("consumer created %d \n", i);	
 			consume(i, maxmsg);
 			exit(0);
 		}	
 	}
+
+	for (i = 0; i < num_p; i++){
+		waitpid(p_pids[i], &status, 0);
+	}
+
+	send_kill_codes(num_c, maxmsg);	
 	
-	while ((wpid == wait(&status)) > 0);	
+	for (i = 0; i < num_c; i++){
+		waitpid(c_pids[i], &status, 0);
+	}	
+	
+
 
 	mq_unlink("/q");	
-/*	for (int i = 0; i < num_c; i++){
-		waitpid(c_pids[i], &status, 2);
-	}
-
-	/*
- 
-
-	child_pid = fork ();
-	if (child_pid != 0){
-		printf("this is the parent process \n");
-	}
-	else{
-		printf("this is the child process \n");
-	}	
-	*/
-
 	gettimeofday(&tv, NULL);
 	g_time[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
 
